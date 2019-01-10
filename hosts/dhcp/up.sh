85e47f3b8e332dc051a076bcf5cd1d5fb6e0df96
Tuan Nguyen <nmtuan.dev@gmail.com>
2019-01-10 15:29:13 +0700

    Finish setting up dhcp host

---
+++ hosts/dhcp/up.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+# Enviroment variables
+CONF=${CONF:-/etc/dhcp/dhcpd.conf}
+INTERFACES=${INTERFACES:-eth0}
+
+# Make sure leases file exists
+touch /var/lib/dhcp/dhcpd.leases
+
+# Check configuration
+if ! /usr/sbin/dhcpd -t $VERSION -q -cf "$CONF" > /dev/null 2>&1; then
+  echo "dhcpd self-test failed. Please fix $CONF."
+  echo "The error was: "
+  /usr/sbin/dhcpd -t -4 -cf "$CONF"
+  exit 1
+fi
+
+# Start dhcpd in foreground mode
+dhcpd -4 -d -f -cf "$CONF" "$INTERFACES"
